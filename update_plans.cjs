4f10badb1bb4a4c8b2557eb050e1259a42287636
TechnoPowerPOS <adtechnopowerpos@hotmail.com>
2026-06-17 10:40:47 +0300

    remove source code

--- update_plans.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('utils/planPermissions.ts', 'utf8');
-data = data.replace(/hasBackup: false,/g, 'hasBackup: false,\n        hasAutoBackup: false,\n        hasZipBackup: false,');
-data = data.replace(/hasBackup: true,/g, 'hasBackup: true,\n        hasAutoBackup: true,\n        hasZipBackup: true,');
-fs.writeFileSync('utils/planPermissions.ts', data);
